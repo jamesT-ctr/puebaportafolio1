a1e5adbf5963f267b4afbc5ac6ba33c2b257fe54
jamesTautiva <152186922+jamesT-ctr@users.noreply.github.com>
2024-08-16 17:14:46 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+
+// menu hamburguesa
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
 
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('active');
+});
+
+
+
+// parallax text
+
+document.addEventListener('scroll', function() {
+    const parallaxText = document.querySelector('.parallax-text');
+    const scrollPosition = window.pageYOffset;
+    const speed = 0.2; // Velocidad del efecto parallax
+    
+    parallaxText.style.transform = 'translateY(' + scrollPosition * speed + 'px)';
 });
